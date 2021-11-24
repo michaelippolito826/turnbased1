9f368761de7e611e725377b0bd40d4730baa49cd
michaelippolito826 <michaelippolito826@gmail.com>
2021-11-23 20:11:36 -0500

    added pictures

--- Content/pizzas.js
+++ Content/pizzas.js
@@ -10,21 +10,21 @@ window.Pizzas = {
   "s001": {
     name: "Slice Samurai",
     type: PizzaTypes.spicy,
-    src: "/images/characters/pizzas/s001.png",
+    src: "/images/characters/pizzas/champs/tile000.png",
     icon: "/images/icons/spicy.png",
     actions: [ "saucyStatus", "clumsyStatus", "damage1" ],
   },
   "v001": {
     name: "Call Me Kale",
     type: PizzaTypes.veggie,
-    src: "/images/characters/pizzas/v001.png",
+    src: "/images/characters/pizzas/champs/tile001.png",
     icon: "/images/icons/veggie.png",
     actions: [ "damage1" ],
   },
   "f001": {
     name: "Portobello Express",
     type: PizzaTypes.fungi,
-    src: "/images/characters/pizzas/f001.png",
+    src: "/images/characters/pizzas/tile002.png",
     icon: "/images/icons/fungi.png",
     actions: [ "damage1" ],
   }
