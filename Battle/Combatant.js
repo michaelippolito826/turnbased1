dd07c35641998b7c17b536c58ee1923757c248cd
michaelippolito826 <70740198+michaelippolito826@users.noreply.github.com>
2021-11-29 15:12:09 -0500

    poison should work

--- Battle/Combatant.js
+++ Battle/Combatant.js
@@ -101,7 +101,7 @@ class Combatant {
     if (this.status?.type === "poison") {
       return [
         { type: "textMessage", text: "Poisoned!" },
-        { type: "stateChange", recover: 5, onCaster: true }
+        { type: "stateChange", damage: 10, onCaster: true }
       ]
     }
     return [];
