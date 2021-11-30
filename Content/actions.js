c45e61a4779f672c68b350d216db73cb517b1ff9
michaelippolito826 <70740198+michaelippolito826@users.noreply.github.com>
2021-11-30 09:27:46 -0500

    poison before damage

--- Content/actions.js
+++ Content/actions.js
@@ -5,8 +5,8 @@ window.Actions = {
     success: [
       { type: "textMessage", text: "{CASTER} uses {ACTION}!"},
       { type: "animation", animation: "glob", color: "#4db560" },
+      { type: "stateChange", status: { type: "poison", expiresIn: 3 } },
       { type: "stateChange", damage: 20},
-      { type: "stateChange", status: { type: "poison", expiresIn: 3 } }
     ]
   },
   saucyStatus: {
