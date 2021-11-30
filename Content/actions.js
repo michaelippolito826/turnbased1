71d37928ddb4b6fb28cda99ad882c6a978bb77da
michaelippolito826 <70740198+michaelippolito826@users.noreply.github.com>
2021-11-30 10:26:26 -0500

    fixed spin animation

--- Content/actions.js
+++ Content/actions.js
@@ -3,30 +3,30 @@ window.Actions = {
     name: "Poison Trail",
     description: "Does minor damage and causes poison (20 + Poison)!",
     success: [
-      { type: "textMessage", text: "{CASTER} uses {ACTION}!"},
+      { type: "textMessage", text: "{CASTER} uses {ACTION}!" },
       { type: "animation", animation: "glob", color: "#4db560" },
       { type: "stateChange", status: null },
       { type: "stateChange", status: { type: "poison", expiresIn: 3 } },
-      { type: "stateChange", damage: 20},
-    ]
+      { type: "stateChange", damage: 20 },
+    ],
   },
   fling: {
     name: "Fling",
     description: "Does major normal damage (50)!",
     success: [
-      { type: "textMessage", text: "{CASTER} uses {ACTION}!"},
-      // { type: "animation", animation: "battle-spin-right"},
-      { type: "stateChange", damage: 50},
-    ]
+      { type: "textMessage", text: "{CASTER} uses {ACTION}!" },
+      { type: "animation", animation: "spin" },
+      { type: "stateChange", damage: 50 },
+    ],
   },
   insanity_potion: {
     name: "Insanity Potion",
     description: "Recover HP for 3 turns and ",
     success: [
-      { type: "textMessage", text: "{CASTER} uses {ACTION}!"},
-      { type: "stateChange", recover: 50, },
-      { type: "textMessage", text: "{CASTER} recovers HP!", },
-    ]
+      { type: "textMessage", text: "{CASTER} uses {ACTION}!" },
+      { type: "stateChange", recover: 50 },
+      { type: "textMessage", text: "{CASTER} recovers HP!" },
+    ],
   },
 
   saucyStatus: {
@@ -34,19 +34,19 @@ window.Actions = {
     description: "Applies the Saucy status",
     targetType: "friendly",
     success: [
-      { type: "textMessage", text: "{CASTER} uses {ACTION}!"},
-      { type: "stateChange", status: { type: "saucy", expiresIn: 3 } }
-    ]
+      { type: "textMessage", text: "{CASTER} uses {ACTION}!" },
+      { type: "stateChange", status: { type: "saucy", expiresIn: 3 } },
+    ],
   },
   clumsyStatus: {
     name: "Olive Oil",
     description: "Slippery mess of deliciousness",
     success: [
-      { type: "textMessage", text: "{CASTER} uses {ACTION}!"},
+      { type: "textMessage", text: "{CASTER} uses {ACTION}!" },
       { type: "animation", animation: "glob", color: "#dafd2a" },
       { type: "stateChange", status: { type: "clumsy", expiresIn: 3 } },
-      { type: "textMessage", text: "{TARGET} is slipping all around!"},
-    ]
+      { type: "textMessage", text: "{TARGET} is slipping all around!" },
+    ],
   },
   //Items
   item_recoverStatus: {
@@ -54,18 +54,18 @@ window.Actions = {
     description: "Feeling fresh and warm",
     targetType: "friendly",
     success: [
-      { type: "textMessage", text: "{CASTER} uses a {ACTION}!"},
+      { type: "textMessage", text: "{CASTER} uses a {ACTION}!" },
       { type: "stateChange", status: null },
-      { type: "textMessage", text: "Feeling fresh!", },
-    ]
+      { type: "textMessage", text: "Feeling fresh!" },
+    ],
   },
   item_recoverHp: {
     name: "Parmesan",
     targetType: "friendly",
     success: [
-      { type:"textMessage", text: "{CASTER} sprinkles on some {ACTION}!", },
-      { type:"stateChange", recover: 10, },
-      { type:"textMessage", text: "{CASTER} recovers HP!", },
-    ]
+      { type: "textMessage", text: "{CASTER} sprinkles on some {ACTION}!" },
+      { type: "stateChange", recover: 10 },
+      { type: "textMessage", text: "{CASTER} recovers HP!" },
+    ],
   },
-}
+};
